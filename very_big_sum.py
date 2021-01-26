8d2f99ebdfbde21fc4c2917a2431753b90b3abf2
lokeshlsk <lokesh.lsk@outlook.com>
2021-01-26 20:32:41 +0530

    completed very big sum problem

--- very_big_sum.py
+++ very_big_sum.py
@@ -1,2 +1,10 @@
-lokesh
-rtgbdrthbetdnbtdyhnetd
+def aVeryBigSum(ar):
+    res = 0
+    for i in ar:
+        res = res + i
+    return res
+
+ar_count = int(input())
+ar = list(map(int, input().rstrip().split()))
+result = aVeryBigSum(ar)
+print(result)
